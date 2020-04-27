97b535a523e2f4ac68c0b13bd9bd6701f8c64e98
Pranav Deshpande <pranavdeshpande12592@gmail.com>
2020-04-26 18:39:26 -0700

    Initial Commit with CocoaPod template

---
+++ Example/Pods/Target Support Files/Pods-SwiftNewLibrary_Tests/Pods-SwiftNewLibrary_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftNewLibrary_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftNewLibrary_Tests
+@end
---
+++ Example/Pods/Target Support Files/SwiftNewLibrary/SwiftNewLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftNewLibrary : NSObject
+@end
+@implementation PodsDummy_SwiftNewLibrary
+@end
